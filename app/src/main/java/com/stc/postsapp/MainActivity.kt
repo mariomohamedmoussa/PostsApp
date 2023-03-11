f2f65eadc3c6780ef380fb78879adf819d743c61
Mohamed Moussa <momoussa.2021@gmail.com>
2023-03-11 04:08:59 +0200

    init app

---
+++ app/src/main/java/com/stc/postsapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.stc.postsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
